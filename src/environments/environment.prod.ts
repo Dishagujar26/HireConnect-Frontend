df0b0178bbed6989702d48bc666e71bf0c258dbe
Dishagujar26 <dishagujar1@gmail.com>
2026-05-17 12:27:48 +0530

    [Disha] feat: dockerize frontend with Nginx for EC2 deployment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  authServiceBaseUrl: 'http://3.26.224.218:8080/api/v1/auth',
+  paymentServiceBaseUrl: 'http://3.26.224.218:8080/api/payments',
+  apiBaseUrl: 'http://3.26.224.218:8080'
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  authServiceBaseUrl: 'http://localhost:8080/api/v1/auth',
-  paymentServiceBaseUrl: 'http://localhost:8080/api/payments',
-  apiBaseUrl: 'http://localhost:8080'
-};
+  authServiceBaseUrl: 'http://3.26.224.218:8080/api/v1/auth',
+  paymentServiceBaseUrl: 'http://3.26.224.218:8080/api/payments',
+  apiBaseUrl: 'http://3.26.224.218:8080'
+};
